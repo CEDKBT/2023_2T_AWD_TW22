668acb4a0a1d13ac5421dc059616c9e3cf816643
ced <155610765+CEDKBT@users.noreply.github.com>
2024-02-15 18:54:30 +0800

    TSA3

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-function moveBox(color) {
-    var box1 = document.getElementById('box1');
-    var box2 = document.getElementById('box2');
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
 
-    box1.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
-    box2.style.transition = 'top 1.5s ease-in-out, background-color 2.5s ease-out';
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
-    if (box1.style.top === '300px') {
-        box1.style.top = '-300px';
-        box2.style.setProperty('--border-color', color);
-        box2.style.backgroundColor = color;
-        box2.style.top = '300px';
-        box1.style.backgroundColor = 'white';
-    } else {
-        box1.style.top = '300px';
-        box1.style.setProperty('--border-color', color);
-        box1.style.backgroundColor = color;
-        box2.style.top = '-900px';
-        box2.style.backgroundColor = 'white';
+function calculateResult() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
     }
 }
